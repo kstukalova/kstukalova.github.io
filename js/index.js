c11828a00a26636791ac0901a549b1adafecf654
Katya Stukalova <katya.stukalova@berkeley.edu>
2016-01-15 18:54:23 -0800

    mobile

--- js/index.js
+++ js/index.js
@@ -34,10 +34,6 @@
 		
 		// Init Skrollr
 		var s = skrollr.init({
-			mobileCheck: function() {
-                //hack - forces mobile version to be off
-                return false;
-            },
 		    render: function(data) {
 		    
 		        //Debugging - Log the current scroll position.

4b95e6cc8531fefa1b15483b3da643cca3782198
Katya Stukalova <katya.stukalova@berkeley.edu>
2016-07-15 19:13:57 -0700

    switerland movile

--- remove_blank_lines.py
+++ remove_blank_lines.py
@@ -1,4 +1,4 @@
 import fileinput
-for line in fileinput.FileInput("index.html",inplace=1):
+for line in fileinput.FileInput("mobile.html",inplace=1):
     if line.rstrip():
         print(line, end='')
